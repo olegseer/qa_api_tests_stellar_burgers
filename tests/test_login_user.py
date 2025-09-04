776b54af0c5a7a55f2132e6c84b7bf6250472ab4
Oleg Shipulev <oshipulev@gmail.com>
2025-09-04 17:18:20 +0300

    добавлены тесты для авторизации

--- tests/test_login_user.py
+++ tests/test_login_user.py
@@ -1,4 +1,5 @@
 import allure
+import pytest
 
 from generators import generate_user_body
 from methods.user_methods import UserMethods
@@ -6,13 +7,27 @@ from methods.user_methods import UserMethods
 
 class TestLoginUser:
 
+    @allure.title('Авторизация  существующего пользователя')
     def test_success_login_user(self, user_data):
         create_body, login_body = user_data
         with allure.step('Регистрация пользователя'):
             UserMethods.create_user(create_body)
         with allure.step('Авторизация пользователя'):
             response = UserMethods.login_user(login_body['email'], login_body['password'])
-        print(response.json()['accessToken'])
         assert response.status_code == 200
         assert response.json()['success'] is True
         assert response.json()['accessToken'] is not None
+
+    @allure.title('Нельзя авторизоваться с неверными почтой или паролем')
+    @pytest.mark.parametrize('invalid_data', ['email', 'password'])
+    def test_login_user_invalid_data_fail(self, user_data, invalid_data):
+        create_body, login_body = user_data
+        with allure.step('Регистрация пользователя'):
+            UserMethods.create_user(create_body)
+        invalid_user = login_body.copy()
+        with allure.step(f'Изменение значения поля {invalid_data} на несуществующее'):
+            invalid_user[invalid_data] = 'invalid'
+        with allure.step('Попытка авторизации пользователя'):
+            response = UserMethods.login_user(invalid_user['email'], invalid_user['password'])
+        assert response.status_code == 401
+        assert response.json()['message'] == 'email or password are incorrect'
